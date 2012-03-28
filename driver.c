29b9981f9b5b2e1cbf831381c8b54a51b579261b
Professor Moriarty <gowtham047@gmail.com>
2012-03-28 13:28:57 -0400

    Instrumentation

--- driver.c
+++ driver.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 #include "header.h"
 
@@ -42,7 +43,13 @@ int main(int argc, char **argv)
     if(i < num_elements)
       {printf("Invalid input\n"); exit(0);}
 
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    double t1=tv.tv_sec+(tv.tv_usec/1000000.0);
     output = pqsort(input, num_elements, num_threads);
+    gettimeofday(&tv, NULL);
+    double t2=tv.tv_sec+(tv.tv_usec/1000000.0);
+    printf("Parallel execution time with %d threads on %d elements is %.6lfs\n",num_threads,num_elements,t2-t1);
     /*printf("\nOutput: ");
     for(i=0;i<num_elements;i++){
         printf("%d ",output[i]);
